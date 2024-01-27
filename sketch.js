e616b5dcd891be2280c898a5ddb3ba14231ce8f8
FerYsunza <58157620+FerYsunza@users.noreply.github.com>
2024-01-26 22:03:56 -0600

    Update sketch.js
    Changing seconds hand for seconds semi circle

--- sketch.js
+++ sketch.js
@@ -4,21 +4,20 @@
 // By Fer Ysunza, 26/01/24.
 //
 
-let initialSecondAngle;
+let initialSecondFraction;
 let startTime;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   startTime = millis(); // Store the start time
-  initialSecondAngle = map(second(), 0, 60, 0, 360) - 90; // Initial angle based on current second
+  initialSecondFraction = second() / 60; // Initial fraction of the minute based on current second
 }
 
 function draw() {
   background(0);
   let h = hour();
   let m = minute();
-  let s = second();
 
   // Drawing the minute semi-circle
   stroke(0, 255, 0);
@@ -32,22 +31,20 @@ function draw() {
   let end2 = map(h % 12, 0, 12, 0, 360);
   arc(width / 2, height / 2, 350, 350, 270, end2 + 270);
 
-  // Calculate the position of the seconds hand
+  // Calculate the position of the seconds semi-circle
   let elapsedTime = millis() - startTime;
-  let secondAngle = initialSecondAngle + map(elapsedTime % 60000, 0, 60000, 0, 360);
+  let currentSecondFraction = initialSecondFraction + (elapsedTime % 60000) / 60000;
+  let secondAngle = map(currentSecondFraction, 0, 1, 0, 360);
 
-  // Drawing the seconds hand
-  push();
-  translate(width / 2, height / 2);
-  rotate(secondAngle);
-  stroke(skyblue);
-  line(0, 0, 100, 0);
-  pop();
+  // Drawing the seconds semi-circle
+  stroke('skyblue');
+  strokeWeight(20);
+  arc(width / 2, height / 2, 450, 450, 270, secondAngle + 270);
 
   // Displaying the digital time
   noStroke();
   fill(255);
   textSize(32);
   textAlign(CENTER, CENTER);
-  text(nf(h, 2) + ':' + nf(m, 2) + ':' + nf(s, 2), width / 2, height / 2);
+  text(nf(h, 2) + ':' + nf(m, 2) + ':' + nf(second(), 2), width / 2, height / 2);
 }
